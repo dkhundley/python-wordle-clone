db3f4850838d38ba69183b1d3024e7b4e9a1dd08
David Hundley <david.k.hundley@gmail.com>
2022-05-15 15:40:19 -0500

    Updating from previous live stream

---
+++ src/test.py
@@ -0,0 +1,13 @@
+import os
+
+
+variable_1 = 5
+variable_2 = 2
+
+def adding_two_numbers(variable_1, variable_2):
+    my_new_number = variable_1 + variable_2
+
+    return my_new_number
+
+
+print(adding_two_numbers(variable_1, variable_2))
